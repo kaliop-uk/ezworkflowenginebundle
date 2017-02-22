7074d22b573564ed0d29ddccaa66dec64c218aa2
gggeek <ggiunta@kaliop.com>
2017-02-22 20:46:55 +0000

    bundle basic structure

---
+++ DependencyInjection/CompilerPass/TaggedServicesCompilerPass.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Kaliop\eZMigrationBundle\DependencyInjection\CompilerPass;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+class TaggedServicesCompilerPass implements CompilerPassInterface
+{
+    /**
+     * @param ContainerBuilder $container
+     */
+    public function process(ContainerBuilder $container)
+    {
+    }
+}
