6233059457ca5df877047711c806347cea38f3e7
gggeek <ggiunta@kaliop.com>
2017-05-16 23:14:12 +0100

    Add 'summary' option to status command

--- Command/StatusCommand.php
+++ Command/StatusCommand.php
@@ -18,6 +18,7 @@ class StatusCommand extends AbstractCommand
     protected function configure()
     {
         $this->setName('kaliop:workflows:status')
+            ->addOption('summary', null, InputOption::VALUE_NONE, "Only print summary information")
             ->setDescription('List the currently executing or already executed workflows')
         ;
     }
@@ -33,8 +34,27 @@ class StatusCommand extends AbstractCommand
             return;
         }
 
+        $summary = array(
+            Migration::STATUS_TODO => array('To do', 0),
+            Migration::STATUS_STARTED => array('Started', 0),
+            Migration::STATUS_DONE => array('Done', 0),
+            Migration::STATUS_SUSPENDED => array('Suspended', 0),
+            Migration::STATUS_FAILED => array('Failed', 0),
+            Migration::STATUS_SKIPPED => array('Skipped', 0),
+            Migration::STATUS_PARTIALLY_DONE => array('Partially done', 0),
+        );
+
         $i = 1;
         foreach ($workflows as $workflow) {
+
+            if (!isset($summary[$workflow->status])) {
+                $summary[$workflow->status] = array($workflow->status, 0);
+            }
+            $summary[$workflow->status][1]++;
+            if ($input->getOption('summary')) {
+                continue;
+            }
+
             switch ($workflow->status) {
                 case Migration::STATUS_FAILED:
                     $name = '<error>' . $workflow->name . '</error>';
@@ -52,9 +72,18 @@ class StatusCommand extends AbstractCommand
 
         }
 
+        if ($input->getOption('summary')) {
+            $output->writeln("\n <info>==</info> Workflows Summary\n");
+            // do not print info about the not yet supported case
+            unset($summary[Migration::STATUS_PARTIALLY_DONE]);
+            $data = $summary;
+            $headers = array('Status', 'Count');
+        } else {
+            $headers = array('#', 'Slot', 'Workflow', 'Executed on', 'Notes');
+        }
         $table = $this->getHelperSet()->get('table');
         $table
-            ->setHeaders(array('#', 'Slot', 'Workflow', 'Executed on', 'Notes'))
+            ->setHeaders($headers)
             ->setRows($data);
         $table->render($output);
     }
