0718714f7b9c01b87663398d1256b9a8f92376e5
Björn Köster <bjoern.koester@mediata.net>
2018-02-28 16:15:51 +0100

    make config for workflow_directory siteaccess aware

--- DependencyInjection/EzWorkflowEngineExtension.php
+++ DependencyInjection/EzWorkflowEngineExtension.php
@@ -6,6 +6,8 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Configuration\SiteAccessAware\ConfigurationProcessor;
+
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -15,14 +17,27 @@ use Symfony\Component\DependencyInjection\Loader;
 class EzWorkflowEngineExtension extends Extension
 {
     /**
-     * {@inheritDoc}
+     * {@inheritdoc}
      */
+    public function getAlias()
+    {
+        return "ez_workflow_engine";
+    }
+
+
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
+        $config        = $this->processConfiguration( $configuration, $configs );
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.yml');
+        $loader = new Loader\YamlFileLoader( $container, new FileLocator( __DIR__ . '/../Resources/config' ) );
+        $loader->load( 'services.yml' );
+        $loader->load( 'default_settings.yml' );
+
+        $processor = new ConfigurationProcessor( $container, 'ez_workflowengine_bundle' );
+        #$processor->mapSetting( 'table_name', $config );
+        $processor->mapSetting( 'workflow_directory', $config );
+        #$processor->mapSetting( 'enable_debug_output', $config );
+        #$processor->mapSetting( 'recursion_limit', $config );
     }
 }
